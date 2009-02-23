e4cec68bb5b528363c7a5bdb7b442c2d7a1845fd
Berk D. Demir <bdd@mindcast.org>
2009-02-23 21:41:07 +0200

    Overhaul the tests to match new vector array and add new tests for complete vector and api coverage.

--- ruby/test.rb
+++ ruby/test.rb
@@ -5,37 +5,67 @@ class TC_FSHP < Test::Unit::TestCase
   def setup
     @test_vectors = [
       { :passwd =>  'test',
-        :opts =>  {:v =>  0, :s =>  '', :r =>  1},
-        :hash =>  '{FSHP0|0|1}qUqP5cyxm6YcTAhz05Hph5gvu9M=' },
+        :opts =>  {:variant =>  0, :salt =>  '', :rounds =>  1},
+        :ciphertext =>  '{FSHP0|0|1}qUqP5cyxm6YcTAhz05Hph5gvu9M=' },
       { :passwd =>  'test',
-        :opts =>  {:v =>  1, :s =>  '12345678', :r =>  4096},
-        :hash =>  '{FSHP1|8|4096}MTIzNDU2NzjTdHcmoXwNc0ff9+ArUHoN0CvlbPZpxFi1C6RDM/MHSA==' },
+        :opts =>  {:variant =>  1, :salt =>  '12345678', :rounds =>  4096},
+        :ciphertext =>  '{FSHP1|8|4096}MTIzNDU2NzjTdHcmoXwNc0ff9+ArUHoN0CvlbPZpxFi1C6RDM/MHSA==' },
       { :passwd =>  'test',
-        :opts =>  {:v =>  2, :s =>  '!@#$%^&*', :r =>  1024},
-        :hash =>  '{FSHP2|8|1024}IUAjJCVeJir9dx/jPTFM5E0FpbGp5JqZ4cO4pf257/DoZ9CNVkYmKwb+V3D4wpkcu87anZ//pPc=' },
+        :opts =>  {:variant =>  2, :salt =>  '!@#$%^&*', :rounds =>  1024},
+        :ciphertext =>  '{FSHP2|8|1024}IUAjJCVeJir9dx/jPTFM5E0FpbGp5JqZ4cO4pf257/DoZ9CNVkYmKwb+V3D4wpkcu87anZ//pPc=' },
       { :passwd =>  'test',
-        :opts =>  {:v =>  3, :s =>  'FSHP', :r =>  512},
-        :hash =>  '{FSHP3|4|512}RlNIUA4i9JgmY1gNlSGLsfd+sz3UwNqadVLRdbP1/sGanLcZoMBUGX4giFdbHiZGVuvs480BWye+yVKjpDlbyVTOoxA=' },
+        :opts =>  {:variant =>  3, :salt =>  'FSHP', :rounds =>  512},
+        :ciphertext =>  '{FSHP3|4|512}RlNIUA4i9JgmY1gNlSGLsfd+sz3UwNqadVLRdbP1/sGanLcZoMBUGX4giFdbHiZGVuvs480BWye+yVKjpDlbyVTOoxA=' },
     ]
   end
   
   def test_crypt
     @test_vectors.each do |v|
-      genhash = FSHP.crypt(v[:passwd],
-                           v[:opts][:s],
-                           0,
-                           v[:opts][:r],
-                           v[:opts][:v])
-      self.assert_equal genhash, v[:hash], \
-                        "FSHP#crypt FAIL: variant #{v[:opts][:v]}"
+      ciphertext = FSHP.crypt(v[:passwd],
+                              v[:opts][:salt],
+                              0, # saltlen
+                              v[:opts][:rounds],
+                              v[:opts][:variant])
+                              
+      self.assert_equal(ciphertext, v[:ciphertext])
     end
   end
   
   def test_check
     @test_vectors.each do |v|
-      self.assert FSHP.check(v[:passwd], v[:hash]), \
-                  "FSHP#check FAIL: variant #{v[:opts][:v]}"
+      self.assert(FSHP.check(v[:passwd], v[:ciphertext]))
     end
+  end
+  
+  def test_unsupported_variant_exception
+    # Variant 255
+    assert_raise ArgumentError do
+      FSHP.crypt('', '', 0, 0, 255)
+    end
+  end
+  
+  def test_default_crypt
+    ciphertext = FSHP.crypt('fshp')
+    meta = ciphertext.match(FSHP::FSHP_REGEX)
+    self.assert(meta)
+    self.assert_equal(meta.length, 5)
+    
+    self.assert_equal(meta[1], '1')    # variant = 1
+    self.assert_equal(meta[2], '8')    # saltlen = 8
+    self.assert_equal(meta[3], '4096') # rounds = 4096
+  end
+  
+  def test_autofix_saltlen_and_rounds
+    passwd = 'fshp'
+    ciphertext = FSHP.crypt(passwd, nil, -1, -1, 1)
+    meta = ciphertext.match(FSHP::FSHP_REGEX)
+    self.assert(meta)
+    self.assert_equal(meta.length, 5)
+    
+    self.assert_equal(meta[2], '0') # fixed -1 saltlen to 0
+    self.assert_equal(meta[3], '1') # fixed -1 rounds  to 1
+    
+    self.assert(FSHP.check(passwd, ciphertext))
   end
 end
 
