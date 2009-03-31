e81cf379d7ce4f9ec2dd7c6525369d6f08c5f65c
Berk D. Demir <bdd@Sumatra.local>
2009-04-01 02:19:29 +0300

    Python: Purely cosmetic changes for full PEP-8 compliance. Nuked trailing spaces.

--- python/fshp.py
+++ python/fshp.py
@@ -3,11 +3,11 @@
 Fairly Secure Hashed Password (FSHP) is a salted, iteratively hashed
 password hashing implementation.
 
-Design principle is similar with PBKDF1 specification in RFC 2898 
+Design principle is similar with PBKDF1 specification in RFC 2898
 (a.k.a: PKCS #5: Password-Based Cryptography Specification Version 2.0)
 
-FSHP allows choosing the salt length, number of iterations and the
-underlying cryptographic hash function among SHA-1 and SHA-2 (256, 384, 512).
+FSHP allows choosing the salt length, number of iterations and the underlying
+cryptographic hash function among SHA-1 and SHA-2 (256, 384, 512).
 
 SECURITY:
 Default FSHP1 uses 8 byte salts, with 4096 iterations of SHA-256 hashing.
@@ -17,14 +17,14 @@ Default FSHP1 uses 8 byte salts, with 4096 iterations of SHA-256 hashing.
   - There are no known attacks against SHA-256 to find collisions with
     a computational effort of fewer than 2^128 operations at the time of
     this release.
-  
+
 BASIC OPERATION:
   >>> fsh = fshp.crypt('OrpheanBeholderScryDoubt')
   >>> print fsh
   {FSHP1|8|4096}GVSUFDAjdh0vBosn1GUhzGLHP7BmkbCZVH/3TQqGIjADXpc+6NCg3g==
   >>> fshp.check('OrpheanBeholderScryDoubt', fsh)
   True
-    
+
 CUSTOMIZING THE CRYPT:
 Let's set a higher password storage security baseline.
   - Increase the salt length from default 8 to 16.
@@ -33,7 +33,8 @@ Let's set a higher password storage security baseline.
 
   >>> fsh = fshp.crypt('ExecuteOrder66', saltlen=16, rounds=8192, variant=3)
   >>> print fsh
-  {FSHP3|16|8192}0aY7rZQ+/PR+Rd5/I9ssRM7cjguyT8ibypNaSp/U1uziNO3BVlg5qPUng+zHUDQC3ao/JbzOnIBUtAeWHEy7a2vZeZ7jAwyJJa2EqOsq4Io=
+  {FSHP3|16|8192}0aY7rZQ+/PR+Rd5/I9ssRM7cjguyT8ibypNaSp/U1uziNO3BVlg5qPUng+z\
+  HUDQC3ao/JbzOnIBUtAeWHEy7a2vZeZ7jAwyJJa2EqOsq4Io=
 
 """
 
@@ -71,37 +72,40 @@ FSHP                            # fshp signature
 $                               # END
 """
 
+
 def crypt(passwd, salt=None, saltlen=8, rounds=4096, variant=1):
     """Return an FSHP hash string of byte string ``passwd``.
-    
+
     ``salt`` is a byte string. If ``None``, a random salt will be
     generated with respect to ``saltlen`` bytes
-    
+
     ``variant`` selects the underlying hash function to be used.
         0: sha1   (not recommended)
         1: sha256 (default)
         2: sha384
         3: sha512
     """
-    
+
     # Type cast to integer.
     saltlen, rounds, variant = map(int, (saltlen, rounds, variant))
-    
+
     # Ensure we have sane values for salt length and rounds.
-    if saltlen < 0: saltlen = 0
-    if rounds  < 1: rounds  = 1
-    
+    if saltlen < 0:
+        saltlen = 0
+    if rounds < 1:
+        rounds = 1
+
     if salt is None:
         salt = ''
         for i in range(saltlen):
             salt += chr(random.randint(0, 255))
     else:
         saltlen = len(salt)
-    
-    algorithm = { 0: 'sha1', 1: 'sha256', 2: 'sha384', 3: 'sha512' }
-    if not algorithm.has_key(variant):
+
+    algorithm = {0: 'sha1', 1: 'sha256', 2: 'sha384', 3: 'sha512'}
+    if variant not in algorithm:
         raise Exception("Unsupported FSHP variant '%d'" % variant)
-        
+
     h = hashlib.new(algorithm[variant])
     h.update(salt + passwd)
     digest = h.digest()
@@ -109,18 +113,19 @@ def crypt(passwd, salt=None, saltlen=8, rounds=4096, variant=1):
         h = hashlib.new(algorithm[variant])
         h.update(digest)
         digest = h.digest()
-        
+
     meta = FSHP_META_FMTSTR % (variant, saltlen, rounds)
-    b64saltdigest = base64.encodestring(salt + digest).replace('\n','')
-    
+    b64saltdigest = base64.encodestring(salt + digest).replace('\n', '')
+
     return meta + b64saltdigest
+
 
 def check(passwd, ciphertext):
     """Check if ``ciphertext`` is an FSHP hash of ``password``.
-    
+
     Return value is boolean.
     """
-    
+
     # Regular expression match. Yes, it's ugly.
     r = re.compile(FSHP_REGEX, re.VERBOSE)
     match = r.match(ciphertext)
